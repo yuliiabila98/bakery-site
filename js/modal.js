c914bc30757fe785a642f51c09d9ecff7ec6b79c
Yuliia <yb@ithillel.ua>
2025-11-24 01:33:39 +0200

    BEM methodology is implemented into site

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
 
-const toggleModal = () => modal.classList.toggle("is-hidden");
+const toggleModal = () => modal.classList.toggle("backdrop--is-hidden");
 
 modalBtnOpen.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
